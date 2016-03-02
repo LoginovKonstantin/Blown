10f4fd0ab5fa001a00c309b401e8a3148b0c3346
egorka59 <eruslanovegor@mail.ru>
2016-03-02 22:08:17 +0500

    parsing file statistic

--- src/main/java/controllers/StatisticController.java
+++ src/main/java/controllers/StatisticController.java
@@ -3,6 +3,11 @@ package controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by Егор on 29.02.2016.
  */
@@ -13,5 +18,29 @@ public class StatisticController {
         private Label distance;
     @FXML
         private Label money;
-        public static String stats = "";
+
+        public static String maxSpeed;
+        public static String maxDistance;
+        public static String maxMoney;
+
+    @FXML
+    private void initialize(){
+        File file = new File("./src/main/resources/files/statistic");
+        try {
+            Scanner out = new Scanner(file);
+            maxSpeed = out.nextLine();
+            maxDistance = out.nextLine();
+            maxMoney= out.nextLine();
+            out.close();
+            speed.setText( maxSpeed + " KM/H " );
+            distance.setText( maxDistance + " KM " );
+            money.setText( maxMoney + " $ " );
+        } catch (FileNotFoundException e) {
+            System.out.println("Ошибка при чтении файла" + e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
