1f2dce6aea2081f15a8e474dbc7d298a5b3b9841
egorka59 <eruslanovegor@mail.ru>
2016-03-02 21:15:33 +0500

    statistic jpg,fxml,configfile

--- src/main/java/controllers/StatisticController.java
+++ src/main/java/controllers/StatisticController.java
@@ -1,7 +1,17 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 /**
  * Created by Егор on 29.02.2016.
  */
 public class StatisticController {
+    @FXML
+        private Label speed;
+    @FXML
+        private Label distance;
+    @FXML
+        private Label money;
+        public static String stats = "";
 }
