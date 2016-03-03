d50f1a0d29497a1f24360fe593c2fa6efef8de08
kotAndEgor <kit4curs@gmail.com>
2016-03-03 20:02:19 +0500

    new game2

--- src/main/java/start/NewGame.java
+++ src/main/java/start/NewGame.java
@@ -15,6 +15,8 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.Random;
+
 /**
  * Created by 4 on 02.03.2016.
  */
@@ -22,22 +24,33 @@ public class NewGame implements ActionListener{
 
     final static int HEIGHT = 1280;
     final static int WIDTH = 680;
+
     final static Image BACKGROUND_IMAGE1 = new Image("/images/road1.jpg");
     final static Image BACKGROUND_IMAGE2 = new Image("/images/road2.jpg");
     final static Image BACKGROUND_IMAGE3 = new Image("/images/road3.jpg");
 
+    private ImageView background1;
+    private ImageView background2;
+    private ImageView background3;
+
+    private Scene sceneNewGame;
+    private Group root;
+
+    double y = 1;
+
+    Timer timerNewGame;
 
     public void showScene(){
-        Timer timerNewGame = new Timer(20, this);
+        timerNewGame = new Timer(100, this);
+        timerNewGame.start();
 
-        ImageView background1 = new ImageView(BACKGROUND_IMAGE1);
-        ImageView background2 = new ImageView(BACKGROUND_IMAGE2);
-        ImageView background3 = new ImageView(BACKGROUND_IMAGE3);
-        Group root = new Group(background1);
-        Scene sceneNewGame = new Scene(root, HEIGHT, WIDTH);
+        background1 = new ImageView(BACKGROUND_IMAGE1);
+        background2 = new ImageView(BACKGROUND_IMAGE2);
+        background3 = new ImageView(BACKGROUND_IMAGE3);
+        root = new Group(background1);
+        sceneNewGame = new Scene(root, HEIGHT, WIDTH);
         MainApp.stage.setTitle("BLOWN");
         MainApp.stage.setScene(sceneNewGame);
-
 
         sceneNewGame.setOnKeyPressed(new EventHandler<KeyEvent>() {
             public void handle(KeyEvent event) {
@@ -52,12 +65,35 @@ public class NewGame implements ActionListener{
                     scene.getStylesheets().add("/styles/main.css");
                     MainApp.stage.setTitle("BLOWN");
                     MainApp.stage.setScene(scene);
+                    timerNewGame.stop();
                 }
             }
         });
     }
 
     public void actionPerformed(ActionEvent e) {
+        if(root.getLayoutY() == 680){
+            root.setClip(background2);
+//            Random r = new Random();
+//            int image = r.nextInt(3);
+//            if(image == 1){
+//
+//                root.setClip(background1);
+//                root.setLayoutY(y+HEIGHT);
+//            }else{
+//                if(image == 2){
+//                    root.setClip(background2);
+//                    root.setLayoutY(y+HEIGHT);
+//                }else{
+//                    root.setClip(background3);
+//                    root.setLayoutY(y+HEIGHT);
+//                }
+//            }
+        }
+
+        root.setLayoutY(y);
+        y+=5;
+        System.out.println(root.getLayoutY());
 
     }
 }
