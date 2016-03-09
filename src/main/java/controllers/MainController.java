dced4345e0238bf90d0ca115529ff9c83cee855c
kotAndEgor <kit4curs@gmail.com>
2016-03-09 21:07:00 +0500

    informations

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -3,8 +3,12 @@ package controllers;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DialogPane;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import start.NewGame;
 import start.Setting;
 import start.Statistic;
@@ -35,5 +39,28 @@ public class MainController {
     public void clickOnStatistic(ActionEvent actionEvent) {
         Statistic statistic = new Statistic();
         statistic.showScene();
+    }
+
+    public void clickOnWhat(ActionEvent actionEvent) {
+        Text headBar = new Text("\n DEVELOPERS\n");
+        headBar.setStyle("-fx-fill: #027093;");
+        Text created = new Text("LOGINOV KONSTANTIN AND ERUSLANOV EGOR.\n" +
+                "CREATED IN 2016.\n" +
+                "RUSSIA, PERM.");
+        created.setStyle("-fx-fill: #027093;");
+
+
+        Alert alertWhat = new Alert(Alert.AlertType.INFORMATION);
+        alertWhat.setTitle("Informations");
+        alertWhat.setHeaderText("DEVELOPERS");
+
+        DialogPane dialogPane = alertWhat.getDialogPane();
+        dialogPane.getStylesheets().add(
+                getClass().getResource("/styles/main.css").toExternalForm());
+        dialogPane.getStyleClass().add("myDialog");
+        dialogPane.setContent(new TextFlow(created));
+        dialogPane.setHeader(new TextFlow(headBar));
+
+        alertWhat.show();
     }
 }
